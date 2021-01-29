3f6b43b7655887bc2bf8c2f21da22e2bf91a5f1b
Maze Peterson <mazecodes@protonmail.com>
2021-01-29 06:58:39 -0800

    Add up and down loggers

--- src/lib/logger.js
+++ src/lib/logger.js
@@ -13,6 +13,36 @@ const error = message => {
   console.log(`${chalk.redBright('[ERROR]')} ${message}`);
 };
 
+/**
+ * Log an up message
+ *
+ * @param {String} website - Website domain to log
+ * @returns {void}
+ *
+ * @example
+ *   up('example.com')
+ */
+const up = website => {
+  console.log(`${chalk.greenBright('[UP]')} It's just you, ${website} is up.`);
+};
+
+/**
+ * Log a down message
+ *
+ * @param {String} website - Website domain to log
+ * @returns {void}
+ *
+ * @example
+ *   down('example.com')
+ */
+const down = website => {
+  console.log(
+    `${chalk.redBright('[DOWN]')} It's not just you, ${website} is down.`
+  );
+};
+
 module.exports = {
   error,
+  up,
+  down,
 };
